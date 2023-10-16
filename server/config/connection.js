3c802f1db605ed5e5bf6f4655965aabe886e6ea2
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-10-16 15:57:36 -0400

    redeploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,5 +6,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pawfectma
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+//help
 module.exports = mongoose.connection;
